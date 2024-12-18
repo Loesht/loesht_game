6492813c141f116c2b1e12baf6d502d674b83783
Loesht <valeriashtin@gmail.com>
2024-12-18 16:30:55 +0300

    snow_fox.py added
    добавлен файл для управления ресурсами и поведение игры

---
+++ snow_fox.py
@@ -0,0 +1,35 @@
+import sys
+
+import pygame
+
+class SnowFox:
+    """Класс для управления ресурсами и поведением игры"""
+
+    def __init__ (self):
+        """Инициализирует игру и создает игровые ресурсы"""
+        pygame.init()
+
+        # Назначение цвета фона
+        self.bg_color = (214, 234, 236)
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Snow Fox")
+
+    def run_game(self):
+        """Запуск основного цикла игры"""
+        while True:
+            # Отслеживание событий клавиатуры и мыши.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # При каждом проходе цикла перерисовывается экран
+            self.screen.fill(self.bg_color)
+
+            # Отображение последнего прорисованного экрана.
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    # Создание экземпляра игры и ее запуск.
+    sf = SnowFox()
+    sf.run_game()
