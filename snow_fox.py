0b3744ca53078df47e59f14ceed73801096a27bc
Loesht <valeriashtin@gmail.com>
2024-12-18 16:56:27 +0300

    settings added and snow_fox changed

--- snow_fox.py
+++ snow_fox.py
@@ -2,17 +2,21 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class SnowFox:
     """Класс для управления ресурсами и поведением игры"""
 
     def __init__ (self):
         """Инициализирует игру и создает игровые ресурсы"""
         pygame.init()
+        self.settings = Settings()
 
         # Назначение цвета фона
         self.bg_color = (214, 234, 236)
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Snow Fox")
 
     def run_game(self):
@@ -24,7 +28,7 @@ class SnowFox:
                     sys.exit()
 
             # При каждом проходе цикла перерисовывается экран
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             # Отображение последнего прорисованного экрана.
             pygame.display.flip()
