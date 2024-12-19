e2d9dca10ce50d9a168e63307740a65094808f44
Loesht <valeriashtin@gmail.com>
2024-12-19 20:52:24 +0300

    refactoring  _check_events

--- fox.py
+++ fox.py
@@ -27,9 +27,9 @@ class Fox():
     def update(self):
         """Обновляет позицию лисы с учетом флагов"""
         # Обновляется атрибут x, не rect
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.fox_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.fox_speed
 
         # Обновление атрибута rect на основании self.x
